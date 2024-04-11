98f46e55342caf8c3a161d614aaf1a51ec2f72a0
TsantaFitia <andrianirinatsantafitiavana@gmail.com>
2024-04-11 23:37:07 +0300

    Avant tosimasos

--- frontend/src/Pages/Tsanta/acceuil.jsx
+++ frontend/src/Pages/Tsanta/acceuil.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function acceuil(){
     return(
         <>
-            <h1>Bonjour</h1>
+            <h1>Bonjour daholo</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo voluptate eius impedit omnis nesciunt recusandae eos sapiente officia cumque aut voluptates deserunt sit, corporis minima vitae nihil, maiores distinctio vel!</p>
             <Link role="button" to="/Bureau_entree">
              Bureau_entree
