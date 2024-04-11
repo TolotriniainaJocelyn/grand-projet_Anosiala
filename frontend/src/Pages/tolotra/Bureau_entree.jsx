497bce18917025f10033393ba6472d1419b17f03
Tolotriniainajocelyn <tolotra.rahaj@gmail.com>
2024-04-11 23:30:30 +0300

    test

--- frontend/src/Pages/tolotra/Bureau_entree.jsx
+++ frontend/src/Pages/tolotra/Bureau_entree.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 function Bureau_entree(){
     return(
         <>
+          <p>n nh h h</p>
             <Link role="button" to="/">
                 acceuil
             </Link>
