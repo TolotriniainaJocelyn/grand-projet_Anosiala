5cd1dc629c67f62453402df42e3c23904dcbe3d8
TsantaFitia <andrianirinatsantafitiavana@gmail.com>
2024-04-29 15:47:19 +0300

    Ajout de bar de navigation

---
+++ frontend/src/Pages/NavBar.jsx
@@ -0,0 +1,31 @@
+// Navbar.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; // Importez votre fichier CSS pour la barre de navigation
+import 'bootstrap/dist/css/bootstrap.min.css';
+function Navbar(){
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <nav className="navbar">
+          <div className="col offset-5">
+            <ul className="navbar-list">
+              <li className="navbar-item">
+                <Link to="/" className="navbar-link">Hospitalisation</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/Bureau_entree" className="navbar-link">Burreau des Entree</Link>
+              </li>
+              <li>
+                  <input type="text" placeholder='Rechercher ...' className='input' />
+                  <button className='button-recherche'>Recherche</button>               
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- frontend/src/Pages/Tsanta/acceuil.jsx
+++ frontend/src/Pages/Tsanta/acceuil.jsx
@@ -5,10 +5,18 @@ import axios from 'axios';
 function Acceuil() {
   const [nom, setNom] = useState('');
   const [age, setAge] = useState('');
+  const [code, setCode] = useState(''); // État pour stocker le code saisi par l'utilisateur
   const [message, setMessage] = useState('');
+  const [showForm, setShowForm] = useState(false); // État pour contrôler la visibilité du formulaire
+  const [showCodeInput, setShowCodeInput] = useState(false); // État pour contrôler la visibilité du champ de code
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+
+    if (code !== '1010') { // Vérifie le code saisi par l'utilisateur
+      setMessage('Code incorrect');
+      return;
+    }
 
     const ressources = {
       nom: nom,
@@ -29,16 +37,25 @@ function Acceuil() {
 
   return (
     <div className="App">
-      <h1>Envoi de données vers PostgreSQL</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="nom">Nom :</label>
-        <input type="text" id="nom" name="nom" value={nom} onChange={(e) => setNom(e.target.value)} required/>
-        <label htmlFor="age">Age :</label>
-        <input type="number" id="age" name="age" value={age} onChange={(e) => setAge(e.target.value)} required/>
-        <button type="submit">Envoyer</button>
-      </form>
+      <Link onClick={() => { setShowForm(true); setShowCodeInput(true); }}>Nouveau Patient</Link> {/* Lien pour afficher le formulaire et le champ de code */}
+      
+      {showForm && ( /* Affichage conditionnel du formulaire en fonction de showForm */
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="nom">Nom :</label>
+          <input type="text" id="nom" name="nom" value={nom} onChange={(e) => setNom(e.target.value)} required/>
+          <label htmlFor="age">Age :</label>
+          <input type="number" id="age" name="age" value={age} onChange={(e) => setAge(e.target.value)} required/>
+          {showCodeInput && ( /* Affichage conditionnel du champ de code en fonction de showCodeInput */
+            <>
+              <label htmlFor="code">Code :</label> {/* Champ pour saisir le code */}
+              <input type="password" id="code" name="code" value={code} onChange={(e) => setCode(e.target.value)} required/>
+            </>
+          )}
+          <button type="submit">Envoyer</button>
+        </form>
+      )}
+
       <div id="message">{message}</div>
-      <Link role="button" to="/bureau_entree">Bureau_entree</Link>
     </div>
   );
 }
